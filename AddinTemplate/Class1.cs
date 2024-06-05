f1a876a5d9790c7ccff905521ed4596ee5d24c52
Nico Zahn <78918634+nzahnHirschmann@users.noreply.github.com>
2024-06-05 10:47:50 +0200

    Add project files.

---
+++ AddinTemplate/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using EPDM.Interop.epdm;
+
+namespace AddinTemplate
+{
+    [Guid("553C3249-903A-4217-BCF8-95F0CD7548E0")]
+    [ComVisible(true)]
+    public class Class1 : IEdmAddIn5
+    {
+        public void GetAddInInfo(ref EdmAddInInfo poInfo, IEdmVault5 poVault, IEdmCmdMgr5 poCmdMgr)
+        {
+            //Specify information to display in the add-in's Properties dialog box
+            poInfo.mbsAddInName = "C# Add-in";
+            poInfo.mbsCompany = "My Company";
+            poInfo.mbsDescription = "Menu add-in that shows a message box.";
+            poInfo.mlAddInVersion = 1;
+
+            //Specify the minimum required version of SolidWorks PDM Professional
+            poInfo.mlRequiredVersionMajor = 6;
+            poInfo.mlRequiredVersionMinor = 4;
+
+            // Register a menu command
+            poCmdMgr.AddCmd(1, "C# Add-in", (int)EdmMenuFlags.EdmMenu_Nothing);
+
+        }
+
+        public void OnCmd(ref EdmCmd poCmd, ref EdmCmdData[] ppoData)
+        {
+
+            // Handle the menu command
+            if (poCmd.meCmdType == EdmCmdType.EdmCmd_Menu)
+            {
+                if (poCmd.mlCmdID == 1)
+                {
+                    System.Windows.Forms.MessageBox.Show("C# Add-in");
+                }
+            }
+        }
+
+    }
+}
---
+++ AddinTemplate/WindowHandle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AddinTemplate
+{
+    //Wrapper class to use SOLIDWORKS PDM Professional as the parent window
+    class WindowHandle : IWin32Window
+    {
+        private IntPtr mHwnd;
+
+        public WindowHandle(int hWnd)
+        {
+            mHwnd = new IntPtr(hWnd);
+        }
+        public IntPtr Handle
+        {
+            get { return mHwnd; }
+        }
+    }
+}
